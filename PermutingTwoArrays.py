ac9fffb6a74c4a5b6cc7786a7adb2e3db5191336
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-05-28 00:05:57 +0530

    Update PermutingTwoArrays.py

--- PermutingTwoArrays.py
+++ PermutingTwoArrays.py
@@ -1,18 +1,28 @@
+# def twoArrays(k, A, B):
+#     A=sorted(A)
+#     B=B[::-1]
+#     a=[0,0]
+#     for i in range(len(A)):
+#         if A[i]+B[i]<k:
+#             a[0] +=1
+#         else:
+#             a[1]+=1
+#     print(A,B)
+#     print(a)
+#     if a[0]==0:
+#         return "Yes"
+#     else:
+#         return 'No'
 def twoArrays(k, A, B):
-    A=sorted(A)
-    B=B[::-1]
-    a=[0,0]
-    for i in range(len(A)):
-        if A[i]+B[i]<k:
-            a[0] +=1
-        else:
-            a[1]+=1
-    print(A,B)
-    print(a)
-    if a[0]==0:
-        return "Yes"
-    else:
-        return 'No'
+    # To maximize each pair, sort the lists in opposite orders
+    A.sort()
+    B.sort(reverse=True)
+    for a,b in zip(A,B):
+        if a+b < k:
+            return 'NO'
+    return 'YES'
+
+
 A=[2,1,3]
 B=[7,8,9]
 k=10
