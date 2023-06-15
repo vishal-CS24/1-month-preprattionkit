f4aaa457bf0b21537dfc1c6bfc4203989561f550
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-06-15 23:52:02 +0530

    Add files via upload

---
+++ reverse_an_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void reverse(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+void printArray(int arr[], int n)
+{
+    cout << "The array is \n"
+         << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    reverse(arr, 5);
+    printArray(arr, 5);
+    return 0;
+}
