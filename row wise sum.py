7baf0e0afe518059056346edb320a43748dcd88c
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-06-19 23:16:28 +0530

    Add files via upload

---
+++ row wise sum.py
@@ -0,0 +1,34 @@
+﻿from sys import stdin
+
+def rowWiseSum(mat, nRows, mCols):
+    #Your code goes here
+    sum = 0
+    for row in mat:
+        for ele in row:
+            sum += ele
+        print(sum, end =' ')
+        sum = 0
+
+#Taking Input Using Fast I/O
+def take2DInput() :
+    li = stdin.readline().rstrip().split(" ")
+    nRows = int(li[0])
+    mCols = int(li[1])
+    
+    if nRows == 0 :
+        return list(), 0, 0
+    
+    mat = [list(map(int, input().strip().split(" "))) for row in range(nRows)]
+    return mat, nRows, mCols
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    mat, nRows, mCols = take2DInput()
+    rowWiseSum(mat, nRows, mCols)
+    print()
+
+    t -= 1
