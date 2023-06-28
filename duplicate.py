cbbe5b2212f67e6f99f7ea9251d603983e6d66c3
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-06-28 23:54:27 +0530

    Add files via upload

---
+++ duplicate.py
@@ -0,0 +1,39 @@
+﻿'''# Python program to print
+# duplicates from a list
+# of integers
+def Repeat(x):
+	_size = len(x)
+	repeated = []
+	for i in range(_size):
+		k = i + 1
+		for j in range(k, _size):
+			if x[i] == x[j] and x[i] not in repeated:
+				repeated.append(x[i])
+	return repeated
+
+# Driver Code
+n=int(input())
+list1 =  [int(x) for x in input().split()]
+print (Repeat(list1))
+	
+# This code is contributed
+# by Sandeep_anand'''
+
+def Repeat(x):
+	_size = len(x)
+	repeated = []
+	for i in range(_size):
+		k = i + 1
+		for j in range(k, _size):
+			if x[i] == x[j] and x[i] not in repeated:
+				repeated.append(x[i])
+	return repeated
+r=1
+p=int(input())
+while r<=p:
+ n=int(input())
+ list1 =  [int(x) for x in input().split()]
+ #rl=Repeat(list1)
+ for rl in Repeat(list1):
+     print(rl)
+ r=r+1
