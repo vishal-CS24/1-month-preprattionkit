410959ee856b8f7974c6cc5495afc6d4a9000f1e
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-03 21:34:12 +0530

    Add files via upload

---
+++ remove chracter.py
@@ -0,0 +1,35 @@
+﻿
+'''from sys import stdin
+
+
+def removeAllOccurrencesOfChar(string, ch) :
+	# Your code goes here
+   for i in range(len(string)):
+      if string[i]==ch:
+          print('',end='')
+      else:
+         print(string[i],end='')
+#main
+string = stdin.readline().strip()
+ch = stdin.readline().strip()[0]
+removeAllOccurrencesOfChar(string, ch)
+
+          OR
+
+'''
+   
+
+
+from sys import stdin
+
+
+def removeAllOccurrencesOfChar(string, ch) :
+	# Your code goes here
+   r=string.replace(ch,'')
+   return r
+#main
+string = stdin.readline().strip()
+ch = stdin.readline().strip()[0]
+ans=removeAllOccurrencesOfChar(string, ch)
+print(ans)
+
