2aa72e31ba509f5764448aeda3b3ba289554e132
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-06 23:10:16 +0530

    Add files via upload

---
+++ unique.py
@@ -0,0 +1,22 @@
+﻿
+def rev(li, l):
+    res = li[0]
+    for i in range(1, l):
+        res = res ^ li[i]
+    return res
+k=1
+p=int(input())
+while k<=p:
+ n = int(input())
+ li = [int(x) for x in input().split()[:n]]
+ print(rev(li, len(li)))
+ print()
+ k=k+1
+
+
+
+
+
+
+
+
