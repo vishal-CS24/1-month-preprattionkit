9d6e7c23f8d09d563670b646028608a170434d25
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-08 23:44:33 +0530

    Add files via upload

---
+++ write and read in a file.py
@@ -0,0 +1,11 @@
+﻿#writing a file
+file=open("newfile.txt","w")
+file.write("The key function for working with files in Python is the open() function.\nThe open() function takes two parameters; filename, and mode.\nThere are four different methods (modes) for opening a file:\nr - Read - Default value. Opens a file for reading, error if the file does not exist\na - Append - Opens a file for appending, creates the file if it does not exist\nw - Write - Opens a file for writing, creates the file if it does not exist\nx - Create - Creates the specified file, returns an error if the file exists")
+file.close()
+print("file created sucessfully")
+#reading a file
+file=open("newfile.txt","r")
+re=file.read()
+print("\n\t\t\t\twe are reading a file\n")
+print(re)
+file.close()
