89dc4418e5a9333f502abb55200d12309e764645
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-10 23:19:33 +0530

    Add files via upload

---
+++ check array rotation.py
@@ -0,0 +1,40 @@
+﻿'''def checkRotate(arr):
+    m = min(arr)
+    print(arr.index(m))
+    
+n=int(input())
+arr = [int(x) for x in input().split()[:n]]
+checkRotate(arr)'''
+
+
+
+
+
+from sys import stdin
+
+def arrayRotateCheck(arr, n):
+    #Your code goes here
+ try:
+    m = min(arr)
+    print(arr.index(m))
+ except:
+    print(0)
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n == 0:
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    arr, n = takeInput()
+    arrayRotateCheck(arr, n)
+
+    t -= 1
