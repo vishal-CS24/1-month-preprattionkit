2dbf7698baef85cbfd6caec9ad813f9732d8f968
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-12 22:32:55 +0530

    Add files via upload

---
+++ rev every string.py
@@ -0,0 +1,13 @@
+﻿def reverseEachWord(str):
+    li = str.split(' ')
+    size = len(li)
+
+    for i in range(size):
+        s = li[i]
+        oSize = len(s)
+        for j in range(oSize-1, -1,-1):
+            print(s[j],end ='')
+        print('', end =' ')
+        
+str = input()
+reverseEachWord(str)
