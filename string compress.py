0666406351c9f95ca4a1b26717dcb818e0d8dd7e
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-15 23:35:08 +0530

    Add files via upload

---
+++ string compress.py
@@ -0,0 +1,29 @@
+﻿def stringCompression(s):
+    i = 0
+    res = []
+    while i < len(s) :
+        j = i + 1
+        c = 1
+        while j < len(s):
+            if s[i] == s[j]:
+                c+=1
+                j+=1
+                
+            else:
+                break
+            
+        if c > 1:
+            res.append(s[i])
+            res.append(c)
+            
+        else:
+            res.append(s[i])
+            
+        i = i + c
+                
+    for i in range(len(res)):
+        print(res[i],end ='')
+           
+
+s = input()
+stringCompression(s)
