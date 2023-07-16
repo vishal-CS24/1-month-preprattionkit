11b8d332199da0e89f88f7dbf994c97e0dffe0d4
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-16 23:22:28 +0530

    Add files via upload

---
+++ check permutation.py
@@ -0,0 +1,43 @@
+﻿def checkPermutation(a, b):
+    li_1 = sorted(a)
+    li_2 = sorted(b)
+    
+    size = len(li_1)
+    
+    for i in range(size):
+        if li_1[i] != li_2[i]:
+            print('false')
+            break
+            
+    else:
+        print('true')
+        
+a = input()
+b = input()
+
+checkPermutation(a, b)
+
+
+
+
+'''
+      OR
+from sys import stdin
+def checkPermutation(a, b):
+    li_1 = sorted(a)
+    li_2 = sorted(b)
+    
+    size = len(li_1)
+    
+    for i in range(size):
+        if li_1[i] != li_2[i]:
+            return 'false'
+            break
+    else:
+         return 'true'
+        
+a = stdin.readline().strip()
+b = stdin.readline().strip()
+
+print(checkPermutation(a, b))'''
+
