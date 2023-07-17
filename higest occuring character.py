ecc57b9ffc78272ac2c0eaa9d49a6b87f5ba4d44
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-17 23:27:53 +0530

    Add files via upload

---
+++ higest occuring character.py
@@ -0,0 +1,35 @@
+﻿# ASCII_SIZE = 256
+
+
+# def highestOccuring(s):
+#     count = [0] * ASCII_SIZE
+#     max = -1
+#     c = ''
+#     for i in s:
+#         count[ord(i)] += 1
+
+#     for i in s:
+#         if max < count[ord(i)]:
+#             max = count[ord(i)]
+#             c = i
+
+#     return c
+
+
+ASCII_SIZE = 256
+
+
+def highestOccuring(s):
+    count = [0]*ASCII_SIZE
+    max = -1
+    ans = ''
+    for i in s:
+        count[ord(i)] += 1
+        if max < count[ord(i)]:
+            max = count[ord(i)]
+            ans = i
+    return ans
+
+
+s = input()
+print(highestOccuring(s))
