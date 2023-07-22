ac63f2313ccfd0f3e61dfbb2d2aed2bb7ee22818
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-07-22 22:41:02 +0530

    Add files via upload

---
+++ generate 2d list.py
@@ -0,0 +1,9 @@
+﻿li=['vishu','rohit','gurjinder','nikit','harsh']
+li1=[[rc for rc in ele]for ele in li]
+print(li1)
+print()
+
+
+li=['vishu','rohit','gurjinder','nikit','harsh']
+li1=[[i for i in li] for ele in li]
+print(li1)
