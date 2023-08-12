a02be0406167ffa005d2a97019d46a5e137bfaaa
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-08-12 23:49:53 +0530

    Add files via upload

---
+++ coffee.py
@@ -0,0 +1,10 @@
+﻿coffee = ["Café Latte", "Caffe Americano", "Espresso", "Cappuccino", "Macchiato"]
+choice = int(input("please enter a number for coffee : "))
+l=len(coffee)
+try:
+	choice < l
+	print("your selected coffee is : "+coffee[choice])
+except:
+	print("Please enter a number between 0 and 4")
+finally:
+	print("Have a good day!")
