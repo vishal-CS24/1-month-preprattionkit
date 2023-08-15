e6d95624326f570e7cd18842df356c5c048947c8
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-08-15 23:37:56 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+﻿'''def printkfreq(a,k):
+	words=a.split()
+	d={}
+	for w in words:
+		d[w]=d.get(w,0)+1
+	for w in d:
+		if d[w]==k:
+			print(w)
+a=input()
+k=int(input())
+printkfreq(a,k)
+
+       OR
+
+'''
+
+def printkfreq(a,k):
+	d={}
+	for w in a:
+		d[w]=d.get(w,0)+1
+	for w in d:
+		if d[w]==k:
+			print(w)
+a=input().split()
+k=int(input())
+printkfreq(a,k)
+
